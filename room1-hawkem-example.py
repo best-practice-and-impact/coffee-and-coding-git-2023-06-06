6da7d9c54598b961b3e61edbdaae3ef3dee5c5d7
hawkem <matthew.hawkes@ons.gov.uk>
2023-06-06 13:43:01 +0100

    Add docstrings/comments

--- room1-hawkem-example.py
+++ room1-hawkem-example.py
@@ -1,4 +1,22 @@
+"""Multiply two values together."""
+
 def multiply_two_inputs(value1, value2):
+    """
+    Multiply two values together and return the result.
+
+    Parameters
+    ----------
+    value1 : int or float
+        First value.
+    value2 : int or float
+        Second value.
+
+    Returns
+    -------
+    int or float
+        The resulting value from multiplying value1 and value2.
+
+    """
     return value1 * value2
 
 
